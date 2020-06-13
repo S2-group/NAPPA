ae8dcad3edc2dab91f567349ce0912da49a65adc
Wesley Shann <sshann95@outlook.com>
2020-06-13 18:49:38 +0200

    Explain methods and utility class via JavaDoc
    S2-group/NAPPA#50

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/util/NappaUtil.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/util/NappaUtil.java
@@ -10,11 +10,25 @@ import nl.vu.cs.s2group.nappa.PrefetchingLib;
 import nl.vu.cs.s2group.nappa.graph.ActivityNode;
 import nl.vu.cs.s2group.nappa.prefetchurl.ParameteredUrl;
 
+/**
+ * This class containing common utility methods for the NAPPA Prefetching Library
+ */
 public class NappaUtil {
+
     private NappaUtil() {
         throw new IllegalStateException("NappaUtil is a utility class and should be instantiated!");
     }
 
+    /**
+     * Parses the list of candidate nodes and verifies if the URLs requested in a candidate node can
+     * be requested with the Extras captured in the visited node
+     *
+     * @param visitedNode    Represents the node which the user is currently visiting
+     * @param candidateNodes Represents a list containing all nodes with the potential to be visited
+     *                       in the near future
+     * @return All URLs requested by all candidate nodes that can be requested with the information
+     * present in the currently visited node
+     */
     @NotNull
     public static List<String> getUrlsFromCandidateNodes(ActivityNode visitedNode, @NotNull List<ActivityNode> candidateNodes) {
         List<String> candidateUrls = new LinkedList<>();
@@ -26,6 +40,15 @@ public class NappaUtil {
         return candidateUrls;
     }
 
+    /**
+     * Verifies if the URLs requested in the candidate node can be requested with the Extras captured
+     * in the visited node
+     *
+     * @param visitedNode   Represents the node which the user is currently visiting
+     * @param candidateNode Represents a node with the potential to be visited in the near future
+     * @return All URLs requested in the candidate node that can be requested with the information
+     * present in the currently visited node
+     */
     @NotNull
     public static List<String> getUrlsFromCandidateNode(@NotNull ActivityNode visitedNode, @NotNull ActivityNode candidateNode) {
         List<String> candidateUrls = new LinkedList<>();
