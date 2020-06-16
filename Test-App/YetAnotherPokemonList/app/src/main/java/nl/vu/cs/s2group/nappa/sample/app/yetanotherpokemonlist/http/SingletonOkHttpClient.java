5f93b60a721b76c7ba4e091958077884d31d95ff
Wesley Shann <sshann95@outlook.com>
2020-06-16 21:35:28 +0200

    Implements a singleton class for OkHttpClient
    [S2-group/NAPPA#57]

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/http/SingletonOkHttpClient.java
@@ -0,0 +1,32 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.http;
+
+import okhttp3.OkHttpClient;
+
+/**
+ * Provides a Singleton instance of {@link OkHttpClient}.
+ * <p>
+ * Read more about Singleton pattern in Android at
+ * https://medium.com/@kevalpatel2106/how-to-make-the-perfect-singleton-de6b951dfdb0
+ */
+public class SingletonOkHttpClient {
+    private static OkHttpClient okHttpClient;
+
+    private SingletonOkHttpClient() {
+        throw new IllegalStateException("SingletonOkHttpClient is a singleton class and should be instantiated via constructor");
+    }
+
+    /**
+     * Instantiate a new {@link OkHttpClient} if invoked by the first time, otherwise, obtain a previously instantiated
+     * object.
+     *
+     * @return A singleton  instance of {@link OkHttpClient}.
+     */
+    public static OkHttpClient getInstance() {
+        if (okHttpClient == null) {
+            okHttpClient = new OkHttpClient();
+        }
+
+        return okHttpClient;
+    }
+
+}
