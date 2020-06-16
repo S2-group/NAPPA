2cf1c51c77a68c292f89814e1b19354c5ea1e5fb
Wesley Shann <sshann95@outlook.com>
2020-06-16 21:15:17 +0200

    Create a new Android project with an empty Activity
    [S2-group/NAPPA#57]

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/MainActivity.java
@@ -0,0 +1,14 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
