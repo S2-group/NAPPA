a2a58b768384e58e4cb3a849a3c01b0da3d0bd15
Wesley Shann <sshann95@outlook.com>
2020-06-16 21:44:28 +0200

    Create a new activity to list pokemons
    [S2-group/NAPPA#57]

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsActivity.java
@@ -0,0 +1,14 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PokemonsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pokemons);
+    }
+}
