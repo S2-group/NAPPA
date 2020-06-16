b30aa249322e4b2841e48e1438964ad96607c3f6
Wesley Shann <sshann95@outlook.com>
2020-06-16 21:49:34 +0200

    Redirect to the PokemonsActivity
    [S2-group/NAPPA#57]

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/MainActivity.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/MainActivity.java
@@ -2,7 +2,10 @@ package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon.PokemonsActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = new Intent(this, PokemonsActivity.class);
+        startActivity(intent);
     }
 }
