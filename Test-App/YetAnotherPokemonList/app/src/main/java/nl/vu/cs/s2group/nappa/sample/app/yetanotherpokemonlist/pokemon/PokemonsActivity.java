e4d1621c63b0806f030cf1fe0e96a4d1da632bbd
Wesley Shann <sshann95@outlook.com>
2020-06-16 22:16:00 +0200

    Make a HTTP request via OkHttpClient
    [S2-group/NAPPA#57]

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsActivity.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsActivity.java
@@ -3,12 +3,21 @@ package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.io.IOException;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.Config;
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.http.SingletonOkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class PokemonsActivity extends AppCompatActivity {
+    private static final String LOG_TAG = PokemonsActivity.class.getSimpleName();
+    private static final String API_URL = Config.API_URL + "pokemon/";
     String[] mobileArray = {"Android", "IPhone", "WindowsMobile", "Blackberry",
             "WebOS", "Ubuntu", "Windows7", "Max OS X"};
 
@@ -21,6 +30,17 @@ public class PokemonsActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
+
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .build();
+
+        try (Response response = SingletonOkHttpClient.getInstance().newCall(request).execute()) {
+            Log.d(LOG_TAG, response.body().string());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, R.layout.activity_listview, mobileArray);
         ListView listView = (ListView) findViewById(R.id.pokemon_list);
         listView.setAdapter(adapter);
