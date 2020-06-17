a094bfdd56e8ae6690c577ea28dd9eabb73d1464
Wesley Shann <sshann95@outlook.com>
2020-06-17 23:57:49 +0200

    Remove leftover class
    Issue: S2-group/NAPPA#57

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokeapi/DefaultApiDefaultList.java
+++
@@ -1,7 +0,0 @@
-package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokeapi;
-
-public class DefaultApiDefaultList extends DefaultApi {
-    private static final String LOG_TAG = DefaultApiDefaultList.class.getSimpleName();
-
-
-}
