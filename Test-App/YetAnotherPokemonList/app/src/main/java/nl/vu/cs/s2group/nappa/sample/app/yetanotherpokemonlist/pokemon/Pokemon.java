0fe0f75bb4b9c6537764ddfb015814da19bec6e9
Wesley Shann <sshann95@outlook.com>
2020-06-17 18:33:06 +0200

    Implement Parcelable in Pokemon model
    Issue: S2-group/NAPPA#57
    
    PagedListAdapter is an implementation of RecyclerView.Adapter that presents data from a PagedList. It uses DiffUtil as a parameter to calculate data differences and do all the updates for you.
    
    The DiffUtil is defined in the Model Class in our case:

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/Pokemon.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/Pokemon.java
@@ -1,20 +1,53 @@
 package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+
+import java.util.Objects;
 
 /**
  * Wrap the entries for the {@code /pokemon} API
  */
-public class Pokemon {
+public class Pokemon implements Parcelable {
+    public static final Creator<Pokemon> CREATOR = new Creator<Pokemon>() {
+        @Override
+        public Pokemon createFromParcel(Parcel in) {
+            return new Pokemon(in);
+        }
+
+        @Override
+        public Pokemon[] newArray(int size) {
+            return new Pokemon[size];
+        }
+    };
+
+    public static DiffUtil.ItemCallback<Pokemon> DIFF_CALLBACK = new DiffUtil.ItemCallback<Pokemon>() {
+        @Override
+        public boolean areItemsTheSame(@NonNull Pokemon oldItem, @NonNull Pokemon newItem) {
+            return oldItem.name.equals(newItem.name);
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Pokemon oldItem, @NonNull Pokemon newItem) {
+            return oldItem.equals(newItem);
+        }
+    };
     /**
      * The name of the pokemon
      */
     String name;
-
     /**
      * The URL link containing information of the pokemon
      */
     String url;
+
+    protected Pokemon(Parcel in) {
+        name = in.readString();
+        url = in.readString();
+    }
 
     @NonNull
     @Override
@@ -28,5 +61,30 @@ public class Pokemon {
 
     public String getUrl() {
         return url;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(name);
+        parcel.writeString(url);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pokemon pokemon = (Pokemon) o;
+        return Objects.equals(name, pokemon.name) &&
+                Objects.equals(url, pokemon.url);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, url);
     }
 }
