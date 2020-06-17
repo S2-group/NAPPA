3cfa41cab1c3220eb202117ea4b80f4886c6a773
Wesley Shann <sshann95@outlook.com>
2020-06-18 00:15:12 +0200

    Create default activity
    Issue: S2-group/NAPPA#57

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokeapi/DefaultActivity.java
@@ -0,0 +1,16 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokeapi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
+
+public class DefaultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_default);
+    }
+}
