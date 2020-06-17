d96fffdd4b6ffc546d0f1539d413f9cac73ee2c2
Wesley Shann <sshann95@outlook.com>
2020-06-17 19:59:44 +0200

    Revert "Revert "Update View Model""
    This reverts commit d276e7c1e7e9856e34d372924f125d0aa7126b7b.

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsViewModel.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsViewModel.java
@@ -9,11 +9,10 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
 
 public class PokemonsViewModel extends ViewModel {
-    private Executor executor;
-    private LiveData<PagedList<Pokemon>> articleLiveData;
+    private LiveData<PagedList<Pokemon>> pokemonLiveData;
 
-    private void init() {
-        executor = Executors.newFixedThreadPool(5);
+    public PokemonsViewModel() {
+        Executor executor = Executors.newFixedThreadPool(5);
 
         PokemonsDataFactory pokemonsDataFactory = new PokemonsDataFactory();
 
@@ -23,12 +22,12 @@ public class PokemonsViewModel extends ViewModel {
                         .setInitialLoadSizeHint(10)
                         .setPageSize(20).build();
 
-        articleLiveData = (new LivePagedListBuilder<>(pokemonsDataFactory, pagedListConfig))
+        pokemonLiveData = (new LivePagedListBuilder<>(pokemonsDataFactory, pagedListConfig))
                 .setFetchExecutor(executor)
                 .build();
     }
 
-    public LiveData<PagedList<Pokemon>> getArticleLiveData() {
-        return articleLiveData;
+    public LiveData<PagedList<Pokemon>> getPokemonLiveData() {
+        return pokemonLiveData;
     }
 }
