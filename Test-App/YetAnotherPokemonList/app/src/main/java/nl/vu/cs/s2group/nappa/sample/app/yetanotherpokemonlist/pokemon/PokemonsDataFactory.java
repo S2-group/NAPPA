31183d75b06298d51d26c31a7dfe327fae05b2a4
Wesley Shann <sshann95@outlook.com>
2020-06-17 18:11:35 +0200

    Create data factory for pokemons
    Issue: S2-group/NAPPA#57

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/PokemonsDataFactory.java
@@ -0,0 +1,28 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.MutableLiveData;
+import androidx.paging.DataSource;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.model.pokemon.Pokemon;
+
+public class PokemonsDataFactory extends DataSource.Factory<Integer, Pokemon> {
+    private MutableLiveData<PokemonDataSource> mutableLiveData;
+    private PokemonDataSource pokemonDataSource;
+
+    public PokemonsDataFactory() {
+        this.mutableLiveData = new MutableLiveData<>();
+    }
+
+    @NonNull
+    @Override
+    public DataSource<Integer, Pokemon> create() {
+        pokemonDataSource = new PokemonDataSource();
+        mutableLiveData.postValue(pokemonDataSource);
+        return pokemonDataSource;
+    }
+
+    public MutableLiveData<PokemonDataSource> getMutableLiveData() {
+        return mutableLiveData;
+    }
+}
