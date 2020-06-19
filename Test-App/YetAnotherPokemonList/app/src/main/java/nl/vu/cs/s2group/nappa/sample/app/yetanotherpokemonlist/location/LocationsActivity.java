fff66ec93c7e98f210582ae86ab37608759378b3
Wesley Shann <sshann95@outlook.com>
2020-06-20 01:14:08 +0200

    Link locations page
    Issue: S2-group/NAPPA#57

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/location/LocationsActivity.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/location/LocationsActivity.java
@@ -1,13 +1,13 @@
 package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.location;
 
+import android.content.Intent;
 import android.util.Log;
 
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
-import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.berry.BerriesActivity;
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.apiresource.named.NamedAPIActivity;
 
 public class LocationsActivity extends NamedAPIActivity {
-    private static final String LOG_TAG = BerriesActivity.class.getSimpleName();
+    private static final String LOG_TAG = LocationsActivity.class.getSimpleName();
     private static final String API_URL = "location/";
 
     public LocationsActivity() {
@@ -27,5 +27,8 @@ public class LocationsActivity extends NamedAPIActivity {
     @Override
     protected void onItemClickListener(String url) {
         Log.d(LOG_TAG, "Clicked on " + url);
+        Intent intent = new Intent(this, LocationActivity.class);
+        intent.putExtra("url", url);
+        startActivity(intent);
     }
 }
