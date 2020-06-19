90d5ae63be102829fae2f35905273f03057d2efa
Wesley Shann <sshann95@outlook.com>
2020-06-19 19:57:07 +0200

    Create ability activity
    Issue: S2-group/NAPPA#57

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/ability/AbilityActivity.java
@@ -0,0 +1,16 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon.ability;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
+
+public class AbilityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ability);
+    }
+}
