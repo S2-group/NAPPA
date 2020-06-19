73fe95062f699b470d2218759706a1af7bd037d5
Wesley Shann <sshann95@outlook.com>
2020-06-19 20:16:47 +0200

    Display ability name and list of pokemons who owns it
    Issue: S2-group/NAPPA#57

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/ability/AbilityActivity.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/ability/AbilityActivity.java
@@ -1,16 +1,37 @@
 package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon.ability;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.util.ViewUtil;
 
 public class AbilityActivity extends AppCompatActivity {
+    Ability ability;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ability);
+        toggleProgressBarVisibility(true);
+        AbilityAPI.makeRequest(getIntent().getStringExtra("url"), this::handleRequest);
+    }
+
+    private void handleRequest(Ability ability) {
+        this.ability = ability;
+        setPageTitle();
+        ViewUtil.addNamedAPIResourceListToUI(this, R.id.ll_ability_pokemons, ability.pokemon, "getPokemon");
+        toggleProgressBarVisibility(false);
+    }
+
+    private void toggleProgressBarVisibility(boolean isVisible) {
+        runOnUiThread(() -> findViewById(R.id.indeterminateBar).setVisibility(isVisible ? ProgressBar.VISIBLE : ProgressBar.GONE));
+    }
+
+    private void setPageTitle() {
+        ((TextView) findViewById(R.id.page_title)).setText(ability.name);
     }
 }
