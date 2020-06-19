73ea997f7a77d255b0e3ff0e5678d3f26ec62b85
Wesley Shann <sshann95@outlook.com>
2020-06-19 23:00:06 +0200

    Map Berries model
    Issue: S2-group/NAPPA#57

---
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/berry/BerryFlavorMap.java
@@ -0,0 +1,30 @@
+package nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.berry;
+
+import androidx.annotation.NonNull;
+
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.apiresource.named.NamedAPIResource;
+
+/**
+ * Representation of https://pokeapi.co/docs/v2#berryflavormap
+ */
+public class BerryFlavorMap {
+    int potency;
+    NamedAPIResource flavor;
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "BerryFlavorMap{" +
+                "potency=" + potency +
+                ", flavor=" + flavor +
+                '}';
+    }
+
+    public int getPotency() {
+        return potency;
+    }
+
+    public NamedAPIResource getFlavor() {
+        return flavor;
+    }
+}
