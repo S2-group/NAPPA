40de8683fe75328cfb999fa99291938a38e43eed
Wesley Shann <sshann95@outlook.com>
2020-06-19 20:56:59 +0200

    Display ability effect changes
    Issue: S2-group/NAPPA#57

--- Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/ability/AbilityActivity.java
+++ Test-App/YetAnotherPokemonList/app/src/main/java/nl/vu/cs/s2group/nappa/sample/app/yetanotherpokemonlist/pokemon/ability/AbilityActivity.java
@@ -7,10 +7,13 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.LinearLayoutCompat;
 
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.R;
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.pokemon.PokemonActivity;
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.util.PokeAPIUtil;
 import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.util.ViewUtil;
+import nl.vu.cs.s2group.nappa.sample.app.yetanotherpokemonlist.util.commonmodels.Effect;
 
 public class AbilityActivity extends AppCompatActivity {
     private static final String LOG_TAG = AbilityActivity.class.getSimpleName();
@@ -26,14 +29,17 @@ public class AbilityActivity extends AppCompatActivity {
 
     private void handleRequest(Ability ability) {
         this.ability = ability;
+
         setPageTitle();
+        toggleProgressBarVisibility(false);
+        setAbilityEffectChange();
+
         ViewUtil.addNamedAPIResourceListToUI(this, R.id.ll_ability_pokemons, ability.pokemon, "getPokemon", (view) -> {
             String url = view.getTag().toString();
             Log.d(LOG_TAG, "Clicked on " + url);
             startActivity(new Intent(this, PokemonActivity.class)
                     .putExtra("url", url));
         });
-        toggleProgressBarVisibility(false);
     }
 
     private void toggleProgressBarVisibility(boolean isVisible) {
@@ -42,5 +48,15 @@ public class AbilityActivity extends AppCompatActivity {
 
     private void setPageTitle() {
         ((TextView) findViewById(R.id.page_title)).setText(ability.name);
+    }
+
+    private void setAbilityEffectChange() {
+        runOnUiThread(() -> {
+            LinearLayoutCompat layout = findViewById(R.id.ll_ability_effect_change);
+            for (AbilityEffectChange effectChange : ability.effectChanges) {
+                Effect effect = PokeAPIUtil.findObjectWithLanguage(effectChange.effectEntries);
+                layout.addView(ViewUtil.createTextView(this, effect.getEffect(), 0.5f));
+            }
+        });
     }
 }
