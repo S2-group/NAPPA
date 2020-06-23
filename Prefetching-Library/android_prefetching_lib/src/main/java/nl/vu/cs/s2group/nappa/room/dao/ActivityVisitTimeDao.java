485db3e0c991722f3493a8a50959019ba57cc5d4
Wesley Shann <sshann95@outlook.com>
2020-06-23 16:19:35 +0200

    Add select queries
    S2-group/NAPPA#56

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/dao/ActivityVisitTimeDao.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/dao/ActivityVisitTimeDao.java
@@ -29,4 +29,18 @@ public interface ActivityVisitTimeDao {
             "WHERE id_activity = :activityId " +
             "AND id_session > (SELECT MAX(id_session) - :lastNSessions FROM pf_activity_visit_time) ")
     List<ActivityVisitTime> getActivityVisitTime(Long activityId, int lastNSessions);
+
+    @Query("SELECT id_activity, id_session, SUM(duration) as duration " +
+            "FROM pf_activity_visit_time " +
+            "WHERE id_activity = :activityId " +
+            "AND id_session > (SELECT MAX(id_session) - :lastNSessions FROM pf_activity_visit_time) " +
+            "GROUP BY id_session")
+    List<ActivityVisitTime> getAggregatedActivityVisitTime(Long activityId, int lastNSessions);
+
+    @Query("SELECT id_activity, SUM(duration) as duration " +
+            "FROM pf_activity_visit_time " +
+            "WHERE id_activity = :activityId " +
+            "AND id_session > (SELECT MAX(id_session) - :lastNSessions FROM pf_activity_visit_time) " +
+            "GROUP BY id_activity")
+    List<ActivityVisitTime> getTotalAggregatedActivityVisitTime(Long activityId, int lastNSessions);
 }
