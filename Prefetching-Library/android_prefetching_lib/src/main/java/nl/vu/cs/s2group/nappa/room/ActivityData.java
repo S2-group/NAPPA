1636fbaa390371b1d6482b39154e25f187d7d13a
Wesley Shann <sshann95@outlook.com>
2020-06-24 11:16:35 +0200

    Annotate PK with non null
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
@@ -1,5 +1,6 @@
 package nl.vu.cs.s2group.nappa.room;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Index;
@@ -13,6 +14,7 @@ public class ActivityData {
      * the Java Language Specification
      */
     @PrimaryKey
+    @NonNull
     @ColumnInfo(name = "activity_name")
     public String activityName;
 
