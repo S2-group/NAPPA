377ec1b7faf1903f0fc674c7723d30623512180b
Wesley Shann <sshann95@outlook.com>
2020-06-24 13:12:00 +0200

    Comment notes to avoid breaking the build
    This reverts commit b5e71367

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/GreedyPrefetchingStrategyOnVisitFrequencyAndTime.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/GreedyPrefetchingStrategyOnVisitFrequencyAndTime.java
@@ -31,26 +31,26 @@ public class GreedyPrefetchingStrategyOnVisitFrequencyAndTime extends AbstractPr
     }
 
     public List<ActivityNode> getMostProbableNodes(ActivityNode node, float parentScore, List<ActivityNode> candidateNodes) {
-        instead of returning a list of nodes, it might be more useful to pass a list of URLs
-        OR we define the array of URLs as this class property?
-
-        BEFORE THE METHOD
-        mantain frequency and time data in memory
-        load data from last N sessions when initializing the library
-        simple access this data here -- > will surely improve reading speed
-
-        IN THIS METHOD
-        obtain the current node URLs
-        if the number of URLs found is smaller than the maximum allowed, prepare to visit the next
-        child
-
-        normalize the frequency and time by dividing the current value by the sum
-        the child score will be the previous score multiplied by the 0.5 time + 0.5 frequency
-        Unless the current node has a single child, the child score will be lower than the current
-        allow to pass the weight as confiuration
-
-        find the child with the highest probability
-        if this child score is higher than the lower bound thrshold, then visit this child
+//        instead of returning a list of nodes, it might be more useful to pass a list of URLs
+//        OR we define the array of URLs as this class property?
+//
+//        BEFORE THE METHOD
+//        mantain frequency and time data in memory
+//        load data from last N sessions when initializing the library
+//        simple access this data here -- > will surely improve reading speed
+//
+//        IN THIS METHOD
+//        obtain the current node URLs
+//        if the number of URLs found is smaller than the maximum allowed, prepare to visit the next
+//        child
+//
+//        normalize the frequency and time by dividing the current value by the sum
+//        the child score will be the previous score multiplied by the 0.5 time + 0.5 frequency
+//        Unless the current node has a single child, the child score will be lower than the current
+//        allow to pass the weight as confiuration
+//
+//        find the child with the highest probability
+//        if this child score is higher than the lower bound thrshold, then visit this child
 
 
         return candidateNodes;
