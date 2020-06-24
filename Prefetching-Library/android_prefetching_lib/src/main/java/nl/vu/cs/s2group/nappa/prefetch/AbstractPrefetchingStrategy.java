ac63223cff58ca83988c3613e56934ef8d1d0b1f
Wesley Shann <sshann95@outlook.com>
2020-06-24 10:35:42 +0200

    Store max URL configuration
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/AbstractPrefetchingStrategy.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/AbstractPrefetchingStrategy.java
@@ -1,13 +1,16 @@
 package nl.vu.cs.s2group.nappa.prefetch;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.util.Map;
 
 public abstract class AbstractPrefetchingStrategy {
     protected int maxNumberOfUrlToPrefetch;
 
-    public AbstractPrefetchingStrategy(Map<PrefetchingStrategyConfigKeys, Object> config) {
-        if (config.containsKey(PrefetchingStrategyConfigKeys.MAX_URL_TO_PREFETCH)) {
+    public AbstractPrefetchingStrategy(@NotNull Map<PrefetchingStrategyConfigKeys, Object> config) {
+        Object data;
 
-        }
+        data = config.get(PrefetchingStrategyConfigKeys.MAX_URL_TO_PREFETCH);
+        if (data != null) maxNumberOfUrlToPrefetch = Integer.parseInt(data.toString());
     }
 }
--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/PrefetchingStrategyConfigKeys.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/prefetch/PrefetchingStrategyConfigKeys.java
@@ -2,8 +2,8 @@ package nl.vu.cs.s2group.nappa.prefetch;
 
 public enum PrefetchingStrategyConfigKeys {
     /**
-     * Defines the maximum number of URLs that will be prefetched in a run of
-     * {@link PrefetchingStrategy#getTopNUrlToPrefetchForNode}
+     * Maps an {@link Integer} defining the maximum number of URLs that will be prefetched
+     * in a run of {@link PrefetchingStrategy#getTopNUrlToPrefetchForNode}.
      */
     MAX_URL_TO_PREFETCH,
 }
