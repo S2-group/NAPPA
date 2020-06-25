bb99f7f4c15d43c1dbdf6e51d74329346492158a
Wesley Shann <sshann95@outlook.com>
2020-06-25 13:19:12 +0200

    Fix aggregate visit time query missing activities and throwing NullPointerException
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeBySession.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeBySession.java
@@ -5,7 +5,7 @@ import androidx.room.DatabaseView;
 
 @DatabaseView("SELECT activity_name AS activityName, SUM(duration) as totalDuration, id_session AS sessionId " +
         "FROM pf_activity_visit_time " +
-        "GROUP BY id_session")
+        "GROUP BY activity_name, id_session")
 public class AggregateVisitTimeBySession {
     @NonNull
     public String activityName;
