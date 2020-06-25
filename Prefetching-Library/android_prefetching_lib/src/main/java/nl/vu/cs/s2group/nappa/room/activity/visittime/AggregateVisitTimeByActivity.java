5a822f092be9f01d00c31e414cc69a09cbc1329a
Wesley Shann <sshann95@outlook.com>
2020-06-25 15:14:30 +0200

    Override equals and hashCode methods
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeByActivity.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeByActivity.java
@@ -1,6 +1,22 @@
 package nl.vu.cs.s2group.nappa.room.activity.visittime;
 
+import java.util.Objects;
+
 public class AggregateVisitTimeByActivity {
     public String activityName;
     public long totalDuration;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AggregateVisitTimeByActivity that = (AggregateVisitTimeByActivity) o;
+        return totalDuration == that.totalDuration &&
+                activityName.equals(that.activityName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(activityName, totalDuration);
+    }
 }
