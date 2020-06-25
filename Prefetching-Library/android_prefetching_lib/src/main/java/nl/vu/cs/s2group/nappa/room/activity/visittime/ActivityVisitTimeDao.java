d6dfbf814496493dcc758c7a28edcae9c01a58e6
Wesley Shann <sshann95@outlook.com>
2020-06-25 22:46:20 +0200

    Fix query returning empty rows
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/ActivityVisitTimeDao.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/ActivityVisitTimeDao.java
@@ -15,16 +15,36 @@ public interface ActivityVisitTimeDao {
      * time per activity and session and filters this View to return only the rows concerning the
      * provided activity and the last N sessions. The result is aggregated again, returning a
      * single object containing only the activity name and the total duration
+     * <p>
+     * The more complex subquery is required instead of a simple {@code (SELECT MAX(id) -
+     * :lastNSessions FROM pf_session)}, because orphan sessions (i.e., sessions without data)
+     * can be registered when:
+     *
+     * <ul>
+     *     <li> An exception takes place after creating the session - Often during development </li>
+     *     <li> The activity with {@code activityName} was not visited in the last N sessions </li>
+     * </ul>
+     * <p>
+     * To address this, the inner subquery filter the view {@link AggregateVisitTimeBySession}
+     * by the queried activity and takes the last 5 entries, representing the last 5 sessions
+     * where this activity was accessed. These 5 sessions can be different than the actual last
+     * five recorded sessions. Then the first query simply takes the subquery result and aggregate
+     * the total duration to return a single row.
      *
      * @param activityName  The activity to search for
-     * @param lastNSessions The number of sessions to take, starting from the current session and before
+     * @param lastNSessions The number N of sessions to take, where N represents the last N
+     *                      sessions that this activity was accessed
      * @return The total aggregate time spend in a given activity for the last N sessions
      */
-    @Query("SELECT activityName, totalDuration " +
-            "FROM AggregateVisitTimeBySession " +
-            "WHERE activityName = :activityName " +
-            "AND sessionId > (SELECT MAX(id) - :lastNSessions FROM pf_session) " +
-            "GROUP BY activityName")
+    @Query("SELECT activityName, SUM(totalDuration) as totalDuration " +
+            "FROM (" +
+            "   SELECT * " +
+            "   FROM AggregateVisitTimeBySession " +
+            "   WHERE activityName = :activityName " +
+            "   GROUP BY sessionId " +
+            "   ORDER BY sessionId " +
+            "   DESC LIMIT :lastNSessions" +
+            ")")
     LiveData<AggregateVisitTimeByActivity> getAggregateVisitTimeByActivity(String activityName, int lastNSessions);
 
     /**
