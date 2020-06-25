ce7e828ba03827de44ddee5f1cf08497860d498b
Wesley Shann <sshann95@outlook.com>
2020-06-25 10:02:29 +0200

    Add javadoc
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/ActivityVisitTimeDao.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/ActivityVisitTimeDao.java
@@ -11,6 +11,16 @@ public interface ActivityVisitTimeDao {
     @Insert
     void insert(ActivityVisitTime activityVisitTime);
 
+    /**
+     * This query takes a Database View with the aggregate time per activity and session and filters
+     * this View to return only the rows concerning the provided activity and the last N sessions.
+     * The result is aggregated again, returning a single object containing only the activity ID and
+     * the total duration
+     *
+     * @param activityId    The activity to search for
+     * @param lastNSessions The number of sessions to take, starting from the current session and before
+     * @return A list of the total aggregate time spend in a given activity for the last N sessions
+     */
     @Query("SELECT activityId, totalDuration " +
             "FROM AggregateVisitTimeBySession " +
             "WHERE activityId = :activityId " +
