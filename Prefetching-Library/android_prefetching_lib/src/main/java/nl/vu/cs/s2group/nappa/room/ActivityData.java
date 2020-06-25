fbb09c56bda218cfb7f20843278844ada04d5bcd
Wesley Shann <sshann95@outlook.com>
2020-06-25 12:41:33 +0200

    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
@@ -1,5 +1,6 @@
 package nl.vu.cs.s2group.nappa.room;
 
+import androidx.annotation.NonNull;
 import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.Index;
@@ -7,11 +8,18 @@ import androidx.room.PrimaryKey;
 
 @Entity(tableName = "pf_activity", indices = @Index(value = {"activity_name"}, unique = true))
 public class ActivityData {
-    @PrimaryKey(autoGenerate = true) public Long id;
-    // activityName is UNIQUE, thus no two rows may have the same activity name
-    @ColumnInfo(name = "activity_name") public String activityName;
+    @PrimaryKey(autoGenerate = true)
+    public Long id;
 
-    public ActivityData(String activityName) {
+    /**
+     * Represent the canonical name of the underlying Activity class as defined by
+     * the Java Language Specification
+     */
+    @NonNull
+    @ColumnInfo(name = "activity_name")
+    public String activityName;
+
+    public ActivityData(@NonNull String activityName) {
         this.activityName = activityName;
     }
 }
