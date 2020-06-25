9da91c5ffad770090195b060341ce0360f207d0e
Wesley Shann <sshann95@outlook.com>
2020-06-25 13:59:20 +0200

    If the entity already contains non null, then the view extends this property
    S2-group/NAPPA#53

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeBySession.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/activity/visittime/AggregateVisitTimeBySession.java
@@ -1,13 +1,11 @@
 package nl.vu.cs.s2group.nappa.room.activity.visittime;
 
-import androidx.annotation.NonNull;
 import androidx.room.DatabaseView;
 
 @DatabaseView("SELECT activity_name AS activityName, SUM(duration) as totalDuration, id_session AS sessionId " +
         "FROM pf_activity_visit_time " +
         "GROUP BY activity_name, id_session")
 public class AggregateVisitTimeBySession {
-    @NonNull
     public String activityName;
     public long totalDuration;
     public long sessionId;
