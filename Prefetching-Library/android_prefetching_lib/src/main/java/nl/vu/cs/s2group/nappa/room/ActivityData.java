033b15a66500be8e753240a13924a65cf7bbfa4f
Wesley Shann <sshann95@outlook.com>
2020-07-08 17:45:24 +0200

    Remove TODO as issue was rejected
    S2-group/NAPPA#81
    
    As described on #87, the activity name can change if developers rename it.
    As such, it is not fit to be a PK.
    If we are able to keep track of renaming occurrences, then we just need to update a single column in a single table in the current schema where we reference activities per ID

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/room/ActivityData.java
@@ -6,17 +6,6 @@ import androidx.room.Entity;
 import androidx.room.Index;
 import androidx.room.PrimaryKey;
 
-// TODO Replace the activity ID for the name as this Entity PK
-//  The activity name represents the canonical name of the activity
-//  (e.g., `com.myapp.MyActivity`), which means that the activity name is unique.
-//  As such, there is not need to have an auto incremental ID as the PK.
-//  Changing this Entity, however, means refactoring all Entities that reference this entity
-//  using the ID and all classes that keeps this data in memory.
-//  The benefit in doing this refactor are:
-//  * Eliminate the maps `ID -> name` and `name -> ID` present in the library
-//  * Eliminates the need to call getActivityNameById() and getActivityIdByName() --> actual names are different
-//  * Improve logs and database debugging --> Logging the message `Navigating from activity 1 to 6` does not adds value
-//  * Eliminates the need of performing `JOIN` operations with this table to get the activity name
 @Entity(tableName = "pf_activity", indices = @Index(value = {"activity_name"}, unique = true))
 public class ActivityData {
     @PrimaryKey(autoGenerate = true)
