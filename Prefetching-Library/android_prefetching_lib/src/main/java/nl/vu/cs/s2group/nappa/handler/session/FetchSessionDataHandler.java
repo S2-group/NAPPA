7b569a3c36e8f50c6de49bc82a53472e735aef61
Wesley Shann <sshann95@outlook.com>
2020-07-09 22:29:10 +0200

    Update JavaDoc
    S2-group/NAPPA#95

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/handler/session/FetchSessionDataHandler.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/handler/session/FetchSessionDataHandler.java
@@ -24,8 +24,9 @@ import nl.vu.cs.s2group.nappa.util.NappaThreadPool;
 
 /**
  * Defines a handler to fetch in the database a object containing the {@link SessionData}
- * for the provided node. After fetching the data, this handler will register the LiveData
- * object for the provide node.
+ * for the provided node. The data contains the count of Source --> Destination edge
+ * visits. After fetching the data, this handler will register the LiveData object
+ * for the provide node to ensure consistency with the database.
  */
 public class FetchSessionDataHandler {
     private static final String LOG_TAG = FetchSessionDataHandler.class.getSimpleName();
