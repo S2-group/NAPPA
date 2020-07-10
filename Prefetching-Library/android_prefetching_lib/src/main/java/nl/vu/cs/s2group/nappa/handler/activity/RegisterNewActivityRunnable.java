5f824c9f61365ea652a3c677473280b140d0b346
Wesley Shann <sshann95@outlook.com>
2020-07-10 18:27:02 +0200

    Properly fix registering LiveData objects on null object
    S2-group/NAPPA#98

--- Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/handler/activity/RegisterNewActivityRunnable.java
+++ Prefetching-Library/android_prefetching_lib/src/main/java/nl/vu/cs/s2group/nappa/handler/activity/RegisterNewActivityRunnable.java
@@ -2,6 +2,10 @@ package nl.vu.cs.s2group.nappa.handler.activity;
 
 import android.util.Log;
 
+import java.util.Date;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.ScheduledThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 import java.util.function.Consumer;
 
 import nl.vu.cs.s2group.nappa.graph.ActivityGraph;
@@ -24,6 +28,7 @@ public class RegisterNewActivityRunnable implements Runnable {
     PrefetchingStrategy strategy;
     ActivityGraph graph;
     Consumer<ActivityData> callback;
+    ScheduledFuture<?> liveDataFetcherScheduler;
 
     public RegisterNewActivityRunnable(String activityName,
                                        PrefetchingStrategy strategy,
@@ -37,11 +42,39 @@ public class RegisterNewActivityRunnable implements Runnable {
 
     @Override
     public void run() {
+        long start = new Date().getTime();
         ActivityData activity = new ActivityData(activityName);
         activity.id = NappaDB.getInstance().activityDao().insert(activity);
         ActivityNode node = graph.getCurrent();
-        if (node != null) FetchActivityLiveDataInfoHandler.run(node, strategy);
 
+        /*
+         * When using the app for the first time, the first activity accessed will trigger
+         * this runnable to register it. However, due to the implementation design, the
+         * current node is set in the graph only after registering the activity. Due to
+         * time constraints, this is a quick fix to schedule the LiveData fetching to a
+         * later time when the method `graph.getCurrent()` return a non-null object.
+         * Since this is a quick fix, we don;t make use of NappaThreadPool class to
+         * ensure that this scheduler will be isolated and encourage to refactor this in
+         * the future.
+         */
+        if (node != null) FetchActivityLiveDataInfoHandler.run(node, strategy);
+        else {
+            liveDataFetcherScheduler = new ScheduledThreadPoolExecutor(1)
+                    .scheduleAtFixedRate(
+                            () -> {
+                                if (graph.getCurrent() != null) {
+                                    FetchActivityLiveDataInfoHandler.run(graph.getCurrent(), strategy);
+                                    liveDataFetcherScheduler.cancel(false);
+                                    Log.d(LOG_TAG, "Current node is now available after " +
+                                            (new Date().getTime() - start) +
+                                            " ms\n" +
+                                            graph.getCurrent());
+                                }
+                            },
+                            0,
+                            200,
+                            TimeUnit.MILLISECONDS);
+        }
         callback.accept(activity);
 
         Log.d(LOG_TAG, "Activity registered " + activity.activityName + " (#" + activity.id + ")");
